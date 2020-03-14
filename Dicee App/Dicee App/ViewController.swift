d1e726c456ce5744d2a8d79433d4d5ac4703eb67
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-03-14 15:01:41 -0700

    iboutlet diceimage view 1 and 2 set image literal to 6 and 2 and alpha 0.5

--- Dicee App/Dicee App/ViewController.swift
+++ Dicee App/Dicee App/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // IBOulet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // = Image Literal
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView1.alpha = 0.5
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        diceImageView2.alpha = 0.5
     }
 
 
