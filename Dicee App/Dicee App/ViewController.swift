890227af91a076673a1cff3650698f3ec995dcc1
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-03-15 22:08:01 -0700

    random dice on press

--- Dicee App/Dicee App/ViewController.swift
+++ Dicee App/Dicee App/ViewController.swift
@@ -13,18 +13,15 @@ class ViewController: UIViewController {
     // IBOulet allows me to reference a UI element
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        // = Image Literal
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView1.alpha = 0.5
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-        diceImageView2.alpha = 0.5
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+
     }
-
-
+    
 }
 
