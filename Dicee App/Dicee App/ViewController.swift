ac45d3166e7d6f93bd5cc0717260a53e8d92e950
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-04-07 21:47:39 -0700

    motion detect

--- Dicee App/Dicee App/ViewController.swift
+++ Dicee App/Dicee App/ViewController.swift
@@ -15,12 +15,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
+        updateDiceImages()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
         let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
         diceImageView1.image = diceArray[Int.random(in: 0...5)]
         diceImageView2.image = diceArray[Int.random(in: 0...5)]
-
     }
     
 }
